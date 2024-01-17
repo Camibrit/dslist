a420739515dc55b0397df014675a770d676e757a
Camibrit <camila-brito10@hotmail.com>
2024-01-17 11:59:14 -0300

    Repository. Controller. Service

---
+++ src/main/java/com/devsuperior/camiladslist/dto/GameMinDTO.java
@@ -0,0 +1,4 @@
+package com.devsuperior.camiladslist.dto;
+
+public class GameMinDTO {
+}
---
+++ src/main/java/com/devsuperior/camiladslist/services/GameService.java
@@ -0,0 +1,9 @@
+package com.devsuperior.camiladslist.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+}
