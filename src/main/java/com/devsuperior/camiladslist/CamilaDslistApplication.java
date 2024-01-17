d56bea4070fd7ea4af4bc72cc1e6a28ac456881d
Camibrit <camila-brito10@hotmail.com>
2024-01-17 09:29:38 -0300

    Project created

---
+++ src/main/java/com/devsuperior/camiladslist/CamilaDslistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.camiladslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilaDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilaDslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/camiladslist/CamilaDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.camiladslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilaDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
