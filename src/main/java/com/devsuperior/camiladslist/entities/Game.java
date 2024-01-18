a93d894ffb0820056c99a0645af87883c0eee2eb
Camibrit <camila-brito10@hotmail.com>
2024-01-18 12:25:06 -0300

    Database sedd update

--- src/main/java/com/devsuperior/camiladslist/entities/Game.java
+++ src/main/java/com/devsuperior/camiladslist/entities/Game.java
@@ -1,9 +1,14 @@
 package com.devsuperior.camiladslist.entities;
 
 
-import jakarta.persistence.*;
-
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
